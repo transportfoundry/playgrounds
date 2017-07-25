d8c27bb1b05c5ae519a4ec45c76eddea85d80b09
Joseph Molloy <joe.m34@gmail.com>
2017-07-25 11:20:11 +0200

    Work on combining externalities

---
+++ ivtExt/src/main/java/playground/jmolloy/externalitiesAnalysis/MeasureExternalities.java
@@ -0,0 +1,135 @@
+package playground.jmolloy.externalitiesAnalysis;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.contrib.emissions.EmissionModule;
+import org.matsim.contrib.emissions.utils.EmissionsConfigGroup;
+import org.matsim.contrib.noise.NoiseConfigGroup;
+import org.matsim.contrib.noise.NoiseModule;
+import org.matsim.contrib.noise.NoiseOfflineCalculation;
+import org.matsim.contrib.noise.data.NoiseContext;
+import org.matsim.contrib.noise.handler.LinkSpeedCalculation;
+import org.matsim.contrib.noise.handler.NoiseTimeTracker;
+import org.matsim.contrib.noise.handler.PersonActivityTracker;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.events.EventsManagerImpl;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.events.algorithms.EventWriterXML;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleUtils;
+import playground.jmolloy.externalitiesAnalysis.handlers.CongestionAggregator;
+import playground.vsp.congestion.handlers.CongestionHandler;
+import playground.vsp.congestion.handlers.CongestionHandlerImplV3;
+
+/**
+ * Created by molloyj on 17.07.2017.
+ */
+public class MeasureExternalities {
+    private final static Logger log = Logger.getLogger(MeasureExternalities.class);
+
+
+ //   final private static String CONFIG_FILE = "output_config.xml"; // "defaultIVTConfig_w_emissions.xml";
+ //   final private static String EVENTS_FILE = "output_events.xml.gz";
+ //   final private static String RUN_FOLDER = "C:\\Users\\molloyj\\Documents\\ARE_SP_2016\\zurich_1pc\\emissions\\example\\";
+
+    final private static String CONFIG_FILE = "defaultIVTConfig_w_emissions.xml"; // "defaultIVTConfig_w_emissions.xml";
+    final private static String EVENTS_FILE = "800.events.xml.gz";
+    final private static String RUN_FOLDER = "C:\\Users\\molloyj\\Documents\\ARE_SP_2016\\zurich_1pc\\";
+
+
+    private Config config;
+    private NoiseContext noiseContext;
+    private NoiseTimeTracker noiseTimeTracker;
+    private EventsManagerImpl eventsManager;
+
+    public static void main(String[] args) {
+        new MeasureExternalities().run();
+    }
+
+    public void run() {
+
+        int bin_size_s = 3600;
+
+        config = ConfigUtils.loadConfig(RUN_FOLDER + CONFIG_FILE, new EmissionsConfigGroup(), new NoiseConfigGroup());
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+        ((NoiseConfigGroup) config.getModules().get(NoiseConfigGroup.GROUP_NAME)).setTimeBinSizeNoiseComputation(bin_size_s);
+
+        eventsManager = new EventsManagerImpl();
+        MatsimEventsReader reader = new MatsimEventsReader(eventsManager);
+
+        CongestionHandler congestionHandler = new CongestionHandlerImplV3(eventsManager, scenario);
+        CongestionAggregator congestionAggregator = new CongestionAggregator(scenario, bin_size_s);
+
+        eventsManager.addHandler(congestionHandler);
+        eventsManager.addHandler(congestionAggregator);
+
+        //emissions
+        setUpVehicles(scenario);
+        //add emissions module
+        EmissionModule emissionModule = new EmissionModule(scenario, eventsManager);
+        setUpNoise(scenario);
+        //add listener to tally emission events by (warm|cold)/link/userGroup/time
+
+        reader.readFile(RUN_FOLDER + EVENTS_FILE);
+
+
+
+        emissionModule.writeEmissionInformation();
+        log.info("Total delay: " + congestionHandler.getTotalDelay());
+        eventsManager.finishProcessing();
+    }
+
+    public void setUpNoise(Scenario scenario) { //taken from NoiseOfflineCalculation
+
+        noiseContext = new NoiseContext(scenario);
+
+        noiseTimeTracker = new NoiseTimeTracker();
+        noiseTimeTracker.setNoiseContext(noiseContext);
+        noiseTimeTracker.setEvents(eventsManager);
+        //noiseTimeTracker.setOutputFilePath(outputFilePath);
+
+        eventsManager.addHandler(noiseTimeTracker);
+
+        if (noiseContext.getNoiseParams().isUseActualSpeedLevel()) {
+            LinkSpeedCalculation linkSpeedCalculator = new LinkSpeedCalculation();
+            linkSpeedCalculator.setNoiseContext(noiseContext);
+            eventsManager.addHandler(linkSpeedCalculator);
+        }
+
+        if (noiseContext.getNoiseParams().isComputePopulationUnits()) {
+            PersonActivityTracker actTracker = new PersonActivityTracker(noiseContext);
+            eventsManager.addHandler(actTracker);
+        }
+    }
+
+
+    private void setUpVehicles(Scenario scenario) {
+
+        VehicleType car = VehicleUtils.getFactory().createVehicleType(Id.create(TransportMode.car, VehicleType.class));
+        car.setMaximumVelocity(60.0 / 3.6);
+        car.setPcuEquivalents(1.0);
+        car.setDescription("BEGIN_EMISSIONSPASSENGER_CAR;petrol (4S);>=2L;PC-P-Euro-1END_EMISSIONS");
+        scenario.getVehicles().addVehicleType(car);
+
+        for (Id<Person> pid : scenario.getPopulation().getPersons().keySet()) {
+            Id<Vehicle> vid = Id.createVehicleId(pid);
+            Vehicle v = scenario.getVehicles().getFactory().createVehicle(vid, car);
+            scenario.getVehicles().addVehicle(v);
+        }
+
+        for (Link l : scenario.getNetwork().getLinks().values()) {
+            l.getAttributes().putAttribute("type", "43");
+
+        }
+    }
+
+}
