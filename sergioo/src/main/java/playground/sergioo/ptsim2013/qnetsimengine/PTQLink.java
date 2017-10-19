8fcce10900577ebb59b5960c1466ae38f824db27
sergio537 <sergio537@gmail.com>
2017-10-19 13:16:03 +0800

    Fixing Generic routes

--- sergioo/src/main/java/playground/sergioo/ptsim2013/qnetsimengine/PTQLink.java
+++ sergioo/src/main/java/playground/sergioo/ptsim2013/qnetsimengine/PTQLink.java
@@ -974,7 +974,7 @@ public class PTQLink implements NetsimLink, TimeVariantLink {
 
 	public boolean hasGreenForToLink(Id<Link> toLinkId){
 		if (this.qSignalizedItem != null){
-			return this.qSignalizedItem.isLinkGreenForToLink(toLinkId);
+			return this.qSignalizedItem.hasGreenForToLink(toLinkId);
 		}
 		return true; //the lane is not signalized and thus always green
 	}
@@ -982,7 +982,7 @@ public class PTQLink implements NetsimLink, TimeVariantLink {
 	public void setSignalStateAllTurningMoves(SignalGroupState state) {
 		this.qSignalizedItem.setSignalStateAllTurningMoves(state);
 		
-		this.thisTimeStepGreen  = this.qSignalizedItem.isLinkGreen();
+		this.thisTimeStepGreen  = this.qSignalizedItem.hasGreenForAllToLinks();
 		// (this is only for capacity accumulation)
 	}
 
@@ -992,7 +992,7 @@ public class PTQLink implements NetsimLink, TimeVariantLink {
 		}
 		this.qSignalizedItem.setSignalStateForTurningMove(state, toLinkId);
 
-		this.thisTimeStepGreen = this.qSignalizedItem.isLinkGreen();
+		this.thisTimeStepGreen = this.qSignalizedItem.hasGreenForAllToLinks();
 		// (this is only for capacity accumulation.  As soon as at least one turning relation is green, the "link" is considered
 		// green).
 	}
